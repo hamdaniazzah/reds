ce78d732f968e8ae4c67085ca2bbee7e7c2ad011
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2021-07-04 14:03:48 +0700

    Update and rename home_1.js to home_2.js

--- bp/home_1.js
+++
@@ -1,6 +0,0 @@
-/* redS Start */
-location.hash.indexOf("#pulsa-reg")>-1&&window.location.replace("/2021/01/pulsa-reguler.html"),
-location.hash.indexOf("#paket-data")>-1&&window.location.replace("/2021/01/paket-data-internet.html"),
-location.hash.indexOf("#token-pln")>-1&&window.location.replace("/2021/01/token-listrik-pln.html"),
-location.hash.indexOf("#pay")>-1&&window.location.replace("/2020/12/payment-method.html");
-/* redS End */
---
+++ bp/home_2.js
@@ -0,0 +1,7 @@
+/* redS Start */
+location.hash.indexOf("#pulsa")>-1&&window.location.replace("/p/pulsa-all-operator.html"),
+location.hash.indexOf("#paket-data")>-1&&window.location.replace("/p/paket-data-internet.html"),
+location.hash.indexOf("#token-pln")>-1&&window.location.replace("/p/token-listrik-pln.html"),
+location.hash.indexOf("#pulsa-jadi-uang")>-1&&window.location.replace("/p/convert-pulsa.html"),
+location.hash.indexOf("#pay")>-1&&window.location.replace("/p/payment-method.html");
+/* redS End */
